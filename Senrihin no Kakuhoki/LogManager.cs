34f3a7adbae33e5446b3c8c72b260d232a0bc301
Xwilarg <xwilarg@yahoo.fr>
2019-06-12 14:55:23 +0200

    Locate game window

---
+++ Senrihin no Kakuhoki/LogManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Senrihin_no_Kakuhoki
+{
+    public class LogManager
+    {
+        public LogManager(LogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+
+        public void DisplayMessage(LogLevel level, string message)
+        {
+            if (level < _logLevel)
+                return;
+            switch (level)
+            {
+                case LogLevel.Debug: Console.ForegroundColor = ConsoleColor.DarkGray; break;
+                case LogLevel.Error: Console.ForegroundColor = ConsoleColor.Red; break;
+            }
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        LogLevel _logLevel;
+
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Error
+        }
+    }
+}
--- Senrihin no Kakuhoki/Program.cs
+++ Senrihin no Kakuhoki/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Senrihin_no_Kakuhoki
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Senrihin_no_Kakuhoki
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            LogManager log = new LogManager(LogManager.LogLevel.Debug);
+            log.DisplayMessage(LogManager.LogLevel.Info, "Looking for game window...");
+            IntPtr window;
+            do
+            {
+                window = FindWindowByCaption(IntPtr.Zero, "Sans titre - Paint");
+                Thread.Sleep(1000);
+            } while (window.ToInt32() == 0);
+            log.DisplayMessage(LogManager.LogLevel.Info, "Game window found!");
+            log.DisplayMessage(LogManager.LogLevel.Debug, "Game window address: 0x" + window.ToString("X"));
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
         }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        private static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
     }
 }
