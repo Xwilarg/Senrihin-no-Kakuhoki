d20fd5bb2de1d9de0641c12fca4ba6d645927cb8
Xwilarg <xwilarg@yahoo.fr>
2019-06-12 16:10:57 +0200

    Find window location and size

--- Senrihin no Kakuhoki/Program.cs
+++ Senrihin no Kakuhoki/Program.cs
@@ -13,18 +13,28 @@ namespace Senrihin_no_Kakuhoki
             IntPtr window;
             do
             {
-                window = FindWindowByCaption(IntPtr.Zero, "Sans titre - Paint");
+                window = FindWindow(IntPtr.Zero, "Sans titre - Paint");
                 Thread.Sleep(1000);
             } while (window.ToInt32() == 0);
-            log.DisplayMessage(LogManager.LogLevel.Info, "Game window found!");
             log.DisplayMessage(LogManager.LogLevel.Debug, "Game window address: 0x" + window.ToString("X"));
+            RECT rect;
+            do
+            {
+                GetWindowRect(window, out rect);
+            } while (rect.top < 0);
+            int sizeX = rect.right - rect.left;
+            int sizeY = rect.bottom - rect.top;
+            log.DisplayMessage(LogManager.LogLevel.Debug, "Window position: (" + rect.left + ";" + rect.top + "), window size: (" + sizeX + ";" + sizeY + ")");
+            log.DisplayMessage(LogManager.LogLevel.Info, "Game window found!");
             while (true)
             {
                 Thread.Sleep(1000);
             }
         }
 
-        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
-        private static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(IntPtr ptr, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
     }
 }
---
+++ Senrihin no Kakuhoki/RECT.cs
@@ -0,0 +1,7 @@
+﻿namespace Senrihin_no_Kakuhoki
+{
+    public struct RECT
+    {
+        public int left, top, right, bottom;
+    }
+}
