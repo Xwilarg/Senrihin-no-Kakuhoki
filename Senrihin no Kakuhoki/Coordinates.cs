a706fff5db592fbee157fb16543081550de6c2bb
Min-Soo KO <min-soo.ko@epitech.eu>
2019-06-23 23:16:23 +0200

    refactoring code, added Coordinates Class, added player number selection option, added image opening with paint

---
+++ Senrihin no Kakuhoki/Coordinates.cs
@@ -0,0 +1,15 @@
+﻿namespace Senrihin_no_Kakuhoki
+{
+    public struct Coordinates
+    {
+        public int StartX;
+        public int StartY;
+        public int EndX;
+        public int EndY;
+        public int Width;
+        public int Height;
+        public int RealStartX;
+        public int SingleWidth;
+        public int PlayerNum;
+    }
+}
--- Senrihin no Kakuhoki/Program.cs
+++ Senrihin no Kakuhoki/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IronOcr;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
@@ -9,58 +10,143 @@ namespace Senrihin_no_Kakuhoki
 {
     class Program
     {
+        private static int GetPlayers(LogManager log)
+        {
+            ConsoleKeyInfo keyInfo;
+            bool isKeyPressed = false;
+            int players = 0;
+            try
+            {
+                do
+                {
+                    keyInfo = Console.ReadKey();
+                    if (keyInfo.Key == ConsoleKey.NumPad1 || keyInfo.Key == ConsoleKey.NumPad2
+                        || keyInfo.Key == ConsoleKey.NumPad3 || keyInfo.Key == ConsoleKey.NumPad4)
+                    {
+                        isKeyPressed = true;
+                        log.DisplayMessage(LogManager.LogLevel.Info, "There are " + keyInfo.Key.ToString().Substring(6) + " player(s) in your session.");
+                    }
+                    Thread.Sleep(1000);
+                } while (isKeyPressed != true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.NumPad1:
+                        players = 1;
+                        break;
+                    case ConsoleKey.NumPad2:
+                        players = 2;
+                        break;
+                    case ConsoleKey.NumPad3:
+                        players = 3;
+                        break;
+                    case ConsoleKey.NumPad4:
+                        players = 4;
+                        break;
+                }
+            }
+            catch (ExternalException)
+            { }
+            return players;
+        }
+        private static Coordinates? GetRect(LogManager log, RECT rectangle)
+        {
+            int sizeX = rectangle.right - rectangle.left;
+            int sizeY = rectangle.bottom - rectangle.top;
+
+            log.DisplayMessage(LogManager.LogLevel.Debug, "Window position: (" + rectangle.left + ";" + rectangle.top + "), window size: (" + sizeX + ";" + sizeY + ")");
+
+            int Players = GetPlayers(log);
+            Coordinates Coords;
+
+            try
+            {
+                Coords.StartX = (int)(sizeX / 4.00);
+                Coords.StartY = (int)(sizeY / 2.62);
+                Coords.EndX = (int)(sizeX / 1.33);
+                Coords.EndY = (int)(sizeY / 2.30);
+                Coords.Width = Coords.EndX - Coords.StartX;
+                Coords.Height = Coords.EndY - Coords.StartY;
+                Coords.SingleWidth = (int)((Coords.EndX - Coords.StartX) / 4.00);
+                Coords.PlayerNum = Players;
+                int middle = (int)((Coords.EndX - Coords.StartX) / 2.00);
+                Coords.RealStartX = (middle - (int)(Coords.SingleWidth * Coords.PlayerNum / 2.00));
+
+                log.DisplayMessage(LogManager.LogLevel.Debug, "Text rect position: (" + Coords.StartX + ";" + Coords.StartY + "), text rect size: (" + Coords.Width + ";" + Coords.Height + ")");
+
+                return Coords;
+            }
+            catch (ExternalException)
+            { }
+            return null;
+        }
+        private static void OpenImage(string pathToFile)
+        {
+            Process imageViewer = new Process();
+            imageViewer.StartInfo.FileName = @"C:\WINDOWS\system32\mspaint.exe";
+            imageViewer.StartInfo.Arguments = pathToFile;
+            imageViewer.Start();
+        }
+        private static void createAndReadImage(LogManager log, IntPtr gameWindow, Coordinates Coords)
+        {
+            int scale = 2;
+            try
+            {
+                AutoOcr ocr = new AutoOcr();
+                Bitmap bmpImage = new Bitmap(Coords.EndX, Coords.EndY, PixelFormat.Format32bppArgb);
+                Graphics gfxBmp = Graphics.FromImage(bmpImage);
+                IntPtr hdcBitmap = gfxBmp.GetHdc();
+                PrintWindow(gameWindow, hdcBitmap, 0);
+                gfxBmp.ReleaseHdc(hdcBitmap);
+                gfxBmp.Dispose();
+
+                log.DisplayMessage(LogManager.LogLevel.Debug, "this is a debug test message");
+
+                Bitmap imageToResize = bmpImage.Clone(new Rectangle(Coords.StartX, Coords.StartY, Coords.Width, Coords.Height), PixelFormat.Format32bppArgb);
+                imageToResize.Save("Sample.bmp");
+                Image image = Image.FromFile("Sample.bmp");
+                Bitmap imageToOCR = new Bitmap(image, Coords.Width * scale, Coords.Height * scale);
+                imageToOCR.Save("SampleOCR.bmp");
+
+                string pathToImage;
+                for (int i = 0; i < Coords.PlayerNum; ++i)
+                {
+                    pathToImage = "SampleOCR." + (i + 1) + ".bmp";
+                    imageToOCR.Clone(new Rectangle(Coords.SingleWidth * i * scale, 0, (Coords.SingleWidth * scale) - 1, Coords.Height * scale), PixelFormat.Format32bppArgb).Save(pathToImage);
+                    log.DisplayMessage(LogManager.LogLevel.Debug, "Text found on image number" + (i + 1) + ": " + ocr.Read(pathToImage).Text);
+                    OpenImage(pathToImage);
+                }
+            }
+            catch (ExternalException)
+            { }
+        }
         private static void Main(string[] args)
         {
             LogManager log = new LogManager(LogManager.LogLevel.Debug);
+
             log.DisplayMessage(LogManager.LogLevel.Info, "Looking for game window...");
-            IntPtr window;
+
+            IntPtr gameWindow;
             do
             {
-                window = FindWindow(IntPtr.Zero, "Wireframe.png");
+                gameWindow = FindWindow(IntPtr.Zero, "Warframe");
                 Thread.Sleep(1000);
-            } while (window.ToInt32() == 0);
-            log.DisplayMessage(LogManager.LogLevel.Debug, "Game window address: 0x" + window.ToString("X"));
-            RECT rect;
+            } while (gameWindow.ToInt32() == 0);
+
+            log.DisplayMessage(LogManager.LogLevel.Info, "Game window found!");
+
+            RECT rectangle;
             do
             {
-                GetWindowRect(window, out rect);
-            } while (rect.top < 0);
-            int sizeX = rect.right - rect.left;
-            int sizeY = rect.bottom - rect.top;
-            log.DisplayMessage(LogManager.LogLevel.Debug, "Window position: (" + rect.left + ";" + rect.top + "), window size: (" + sizeX + ";" + sizeY + ")");
-            int startX = sizeX / 4;
-            int startY = (int)(sizeY / 2.57);
-            int textSizeX = (int)(sizeX / 1.33);
-            int textSizeY = (int)(sizeY / 2.27);
-            int textFinalSizeX = textSizeX - startX;
-            int textFinalSizeY = textSizeY - startY;
-            log.DisplayMessage(LogManager.LogLevel.Debug, "Text rect position: (" + startX + ";" + startY + "), text rect size: (" + textFinalSizeX + ";" + textFinalSizeY + ")");
-            log.DisplayMessage(LogManager.LogLevel.Info, "Game window found!");
-            AutoOcr ocr = new AutoOcr();
-            while (true)
-            {
-                try
-                {
-                    Bitmap bmp = new Bitmap(textSizeX, textSizeY, PixelFormat.Format32bppArgb);
-                    Graphics gfxBmp = Graphics.FromImage(bmp);
-                    IntPtr hdcBitmap = gfxBmp.GetHdc();
-                    PrintWindow(window, hdcBitmap, 0);
-                    gfxBmp.ReleaseHdc(hdcBitmap);
-                    gfxBmp.Dispose();
-                    Bitmap finalBmp = bmp.Clone(new Rectangle(startX, startY, textFinalSizeX, textFinalSizeY), PixelFormat.Format32bppArgb);
-                    int splitSizeX = textFinalSizeX / 4;
-                    finalBmp.Clone(new Rectangle(0, 0, splitSizeX, textFinalSizeY), PixelFormat.Format32bppArgb).Save("Image1.bmp");
-                    finalBmp.Clone(new Rectangle(splitSizeX, 0, splitSizeX, textFinalSizeY), PixelFormat.Format32bppArgb).Save("Image2.bmp");
-                    finalBmp.Clone(new Rectangle(splitSizeX * 2, 0, splitSizeX, textFinalSizeY), PixelFormat.Format32bppArgb).Save("Image3.bmp");
-                    finalBmp.Clone(new Rectangle(splitSizeX * 3, 0, splitSizeX, textFinalSizeY), PixelFormat.Format32bppArgb).Save("Image4.bmp");
-                    log.DisplayMessage(LogManager.LogLevel.Debug, "Text found on first pannel: " + ocr.Read("Image1.bmp").Text);
-                    log.DisplayMessage(LogManager.LogLevel.Debug, "Text found on second pannel: " + ocr.Read("Image2.bmp").Text);
-                    log.DisplayMessage(LogManager.LogLevel.Debug, "Text found on third pannel: " + ocr.Read("Image3.bmp").Text);
-                    log.DisplayMessage(LogManager.LogLevel.Debug, "Text found on fourth pannel: " + ocr.Read("Image4.bmp").Text);
-                } catch (ExternalException)
-                { }
-                Thread.Sleep(5000);
-            }
+                GetWindowRect(gameWindow, out rectangle);
+            } while (rectangle.top < 0);
+
+            log.DisplayMessage(LogManager.LogLevel.Debug, "Game window address: 0x" + gameWindow.ToString("X"));
+
+            var Coordinates = GetRect(log, rectangle);
+            if (Coordinates != null)
+                createAndReadImage(log, gameWindow, (Coordinates)Coordinates);
+            while(true)
+                Thread.Sleep(15000);
         }
 
         [DllImport("user32.dll")]
